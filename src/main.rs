c0920870196e87686032002529db6699aae2d88e
0xNOY <noy@abc-net.jp>
2022-12-24 12:20:13 +0900

    ロガーを外部ライブラリに変更

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,55 @@
+use dirs;
+use log::{error, info};
+use once_cell::sync::Lazy;
 use reqwest;
-use std::env;
-
-mod log;
+use simplelog::{ColorChoice, CombinedLogger, LevelFilter, TermLogger, TerminalMode, WriteLogger};
+use std::{env, fs::File, path::PathBuf, process::exit};
 
 const URL: &str = "https://dhcp.tamagawa.ac.jp/index.cgi";
 const TIMEOUT_SECS: u64 = 6;
 
+static APP_DATA_DIR_PATH: Lazy<PathBuf> = Lazy::new(|| match dirs::home_dir() {
+    Some(d) => d.join(".tmgw-wifi-authorizer"),
+    None => panic!("ホームディレクトリのパスを取得できません。"),
+});
+static LOG_FILE_PATH: Lazy<PathBuf> = Lazy::new(|| APP_DATA_DIR_PATH.join("log.txt"));
+
 fn main() {
-    let logger = log::Log::new();
-    macro_rules! exit_with_record_error {
-        ($($arg:tt)*) => {{
-            logger.record_error(&std::format!($($arg)*));
-            logger.save();
-            std::process::exit(1);
-        }};
+    match CombinedLogger::init(vec![
+        TermLogger::new(
+            LevelFilter::Info,
+            simplelog::Config::default(),
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        ),
+        WriteLogger::new(
+            LevelFilter::Off,
+            simplelog::Config::default(),
+            match File::create(LOG_FILE_PATH.clone()) {
+                Ok(f) => f,
+                Err(e) => panic!("ログファイルの作成に失敗しました。詳細: {}", e),
+            },
+        ),
+    ]) {
+        Ok(_) => (),
+        Err(e) => panic!("ロガーの初期化に失敗しました。詳細: {}", e),
     }
 
-    logger.record_info("ログインしています。");
+    info!("ログインしています。");
 
     let user_id = match env::var("TMGW_ID") {
         Ok(s) => s,
-        Err(_) => exit_with_record_error!("環境変数 TMGW_ID を定義してください。"),
+        Err(_) => {
+            error!("環境変数 TMGW_ID を定義してください。");
+            exit(1)
+        }
     };
     let user_password = match env::var("TMGW_PASSWORD") {
         Ok(s) => s,
-        Err(_) => exit_with_record_error!("環境変数 TMGW_PASSWORD を定義してください。"),
+        Err(_) => {
+            error!("環境変数 TMGW_PASSWORD を定義してください。");
+            exit(1)
+        }
     };
 
     let post_form_data = [("STAT", "1"), ("USER", &user_id), ("PASS", &user_password)];
@@ -37,22 +62,26 @@ fn main() {
         .send()
     {
         Ok(r) => r,
-        Err(e) => exit_with_record_error!("ログインページへの接続に失敗しました。詳細: {}", e),
+        Err(e) => {
+            error!("ログインページへの接続に失敗しました。詳細: {}", e);
+            exit(1)
+        }
     };
 
     let res_body_str = match res.text_with_charset("EUC-JP") {
         Ok(s) => s,
         Err(_) => {
-            exit_with_record_error!("サーバからのメッセージに不適切なバイト列が含まれています。")
+            error!("サーバからのメッセージに不適切なバイト列が含まれています。");
+            exit(1)
         }
     };
     if res_body_str.contains("認証に成功しました。") {
-        logger.record_info("ログインが完了しました。");
+        info!("ログインが完了しました。");
     } else if res_body_str.contains("ユーザ名かパスワードが間違っています。") {
-        exit_with_record_error!("IDまたはパスワードが異なります。")
+        error!("IDまたはパスワードが異なります。");
+        exit(1)
     } else {
-        exit_with_record_error!("非予期のエラーが発生しました。")
+        error!("非予期のエラーが発生しました。");
+        exit(1)
     }
-
-    logger.save();
 }
