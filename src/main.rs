a1b1aba25fcc08227cbf801f407a4e7482698da3
0xNOY <noy@abc-net.jp>
2023-04-24 16:09:26 +0900

    feat: CLI implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use dirs;
 use log::{error, info};
 use once_cell::sync::Lazy;
@@ -5,25 +6,63 @@ use reqwest;
 use simplelog::{ColorChoice, CombinedLogger, LevelFilter, TermLogger, TerminalMode, WriteLogger};
 use std::{
     env,
-    fs::{create_dir_all, File},
+    fs::{create_dir_all, OpenOptions},
     path::PathBuf,
     process::exit,
 };
-
-const URL: &str = "https://dhcp.tamagawa.ac.jp/index.cgi";
-const TIMEOUT_SECS: u64 = 6;
 
 static APP_DATA_DIR_PATH: Lazy<PathBuf> = Lazy::new(|| match dirs::home_dir() {
     Some(d) => d.join(".tmgw-wifi-authorizer"),
     None => panic!("ホームディレクトリのパスを取得できません。"),
 });
-static LOG_FILE_PATH: Lazy<PathBuf> = Lazy::new(|| APP_DATA_DIR_PATH.join("log.txt"));
+
+#[derive(Debug, Parser)]
+#[clap(
+    name = env!("CARGO_PKG_NAME"),
+    version = env!("CARGO_PKG_VERSION"),
+    author = env!("CARGO_PKG_AUTHORS"),
+    about = env!("CARGO_PKG_DESCRIPTION"),
+    arg_required_else_help = true
+)]
+struct Args {
+    // MyPCアカウントのID
+    #[arg(short = 'n', long, env = "TMGW_ID")]
+    user_id: String,
+
+    // MyPCアカウントのパスワード
+    #[arg(short = 'p', long, env = "TMGW_PASSWORD")]
+    user_password: String,
+
+    // 認証サーバのURL
+    #[arg(
+        short = 'u',
+        long,
+        default_value = "https://dhcp.tamagawa.ac.jp/index.cgi"
+    )]
+    url: String,
+
+    // タイムアウト時間(秒)
+    #[arg(short = 't', long, default_value = "6")]
+    timeout_secs: u64,
+
+    // データディレクトリのパス
+    #[arg(short = 'd', long, default_value = APP_DATA_DIR_PATH.to_str().unwrap())]
+    data_dir_path: PathBuf,
+
+    // ログファイルの名前
+    #[arg(short = 'l', long, default_value = "log.txt")]
+    log_file_name: String,
+}
 
 fn main() {
+    let args = Args::parse();
+
     match create_dir_all(APP_DATA_DIR_PATH.clone()) {
         Ok(_) => (),
         Err(e) => panic!("データディレクトリの作成に失敗しました。詳細: {}", e),
     }
+
+    let log_file_path = args.data_dir_path.join(args.log_file_name);
 
     match CombinedLogger::init(vec![
         TermLogger::new(
@@ -35,9 +74,13 @@ fn main() {
         WriteLogger::new(
             LevelFilter::Trace,
             simplelog::Config::default(),
-            match File::create(LOG_FILE_PATH.clone()) {
+            match OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open(log_file_path)
+            {
                 Ok(f) => f,
-                Err(e) => panic!("ログファイルの作成に失敗しました。詳細: {}", e),
+                Err(e) => panic!("ログファイルの作成・書き込みに失敗しました。詳細: {}", e),
             },
         ),
     ]) {
@@ -47,28 +90,27 @@ fn main() {
 
     info!("ログインしています。");
 
-    let user_id = match env::var("TMGW_ID") {
-        Ok(s) => s,
-        Err(_) => {
-            error!("環境変数 TMGW_ID を定義してください。");
-            exit(1)
-        }
-    };
-    let user_password = match env::var("TMGW_PASSWORD") {
-        Ok(s) => s,
-        Err(_) => {
-            error!("環境変数 TMGW_PASSWORD を定義してください。");
-            exit(1)
-        }
-    };
+    if args.user_id.is_empty() {
+        error!("環境変数 TMGW_ID を定義してください。");
+    }
+    if args.user_password.is_empty() {
+        error!("環境変数 TMGW_PASSWORD を定義してください。");
+    }
+    if args.user_id.is_empty() || args.user_password.is_empty() {
+        exit(1);
+    }
 
-    let post_form_data = [("STAT", "1"), ("USER", &user_id), ("PASS", &user_password)];
+    let form_data = [
+        ("STAT", "1"),
+        ("USER", &args.user_id),
+        ("PASS", &args.user_password),
+    ];
 
     let client = reqwest::blocking::Client::new();
     let res = match client
-        .post(URL)
-        .form(&post_form_data)
-        .timeout(std::time::Duration::from_secs(TIMEOUT_SECS))
+        .post(args.url)
+        .form(&form_data)
+        .timeout(std::time::Duration::from_secs(args.timeout_secs))
         .send()
     {
         Ok(r) => r,
